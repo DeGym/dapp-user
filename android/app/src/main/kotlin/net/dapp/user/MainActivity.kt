bea134850dd1eff28d82d41f1f41bf0e5ebc62df
Enzo D <enzo.danjour@gmail.com>
2024-12-08 01:52:01 -0300

    set locations

---
+++ android/app/src/main/kotlin/net/dapp/user/MainActivity.kt
@@ -0,0 +1,5 @@
+package net.dapp.user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,149 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
+import 'package:material_floating_search_bar_2/material_floating_search_bar_2.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          const MapWidget(),
+          buildFloatingSearchBar(),
+        ],
+      ),
+    );
+  }
+
+  Widget buildFloatingSearchBar() {
+    final isPortrait =
+        MediaQuery.of(context).orientation == Orientation.portrait;
+
+    return FloatingSearchBar(
+      hint: 'Search...',
+      scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
+      transitionDuration: const Duration(milliseconds: 800),
+      transitionCurve: Curves.easeInOut,
+      physics: const BouncingScrollPhysics(),
+      axisAlignment: isPortrait ? 0.0 : -1.0,
+      openAxisAlignment: 0.0,
+      width: isPortrait ? 600 : 500,
+      debounceDelay: const Duration(milliseconds: 500),
+      onQueryChanged: (query) {
+        print(query);
+        // Call your model, bloc, controller here.
+      },
+      // Specify a custom transition to be used for
+      // animating between opened and closed stated.
+      transition: CircularFloatingSearchBarTransition(),
+      actions: [
+        FloatingSearchBarAction(
+          showIfOpened: false,
+          child: CircularButton(
+            icon: const Icon(Icons.place),
+            onPressed: () {},
+          ),
+        ),
+        FloatingSearchBarAction.searchToClear(
+          showIfClosed: false,
+        ),
+      ],
+      builder: (context, transition) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Material(
+            color: Colors.white,
+            elevation: 4.0,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: Colors.accents.map((color) {
+                return Container(height: 112, color: color);
+              }).toList(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MapWidget extends StatelessWidget {
+  const MapWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+      options: const MapOptions(
+        initialCenter:
+            LatLng(-23.533773, -46.625290), // Center the map over London
+        initialZoom: 13,
+      ),
+      children: [
+        TileLayer(
+          // Display map tiles from any source
+          urlTemplate:
+              "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", // OSMF's Tile Server
+          userAgentPackageName: 'com.example.app',
+          retinaMode: true,
+
+          // And many more recommended properties!
+        ),
+        MarkerLayer(
+          markers: [
+            Marker(
+              width: 30.0,
+              height: 30.0,
+              point: LatLng(-23.533773, -46.625290),
+              child: Container(
+                child: IconButton(
+                  icon: Icon(Icons.location_on),
+                  color: Colors.red,
+                  iconSize: 45.0,
+                  onPressed: () {
+                    launch(
+                        'https://www.google.com/maps/search/?api=1&query=-23.533773,-46.625290');
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
+        
+      ],
+    );
+  }
+}
